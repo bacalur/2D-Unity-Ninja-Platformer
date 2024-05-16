fed9fb2c507747f2f831bc0a8c87e2f0bf01f722
bacalur <128888984+bacalur@users.noreply.github.com>
2024-05-16 21:13:37 +0300

    Update Movement.cs

--- Assets/Scripts/Hero/Movement.cs
+++ Assets/Scripts/Hero/Movement.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;          // Скорость перемещения персонажа
-    public float jumpForce = 400f;     // Сила прыжка персонажа
-    public Transform groundCheck;      // Точка, где проверяется, на земле ли находится персонаж
-    public LayerMask groundLayer;      // Слой, на котором находится земля
+    public float speed = 10f;          
+    public float jumpForce = 400f;     
+    public Transform groundCheck;      // Check if the character is on the ground
+    public LayerMask groundLayer;      // The layer where the ground is located
     public float HorizontalMove = 0f;  
 
     private Rigidbody2D rb;
 
     [Header("Player Animation Settings")]
-    public Animator animator;          // Ссылка на компонент Animator
+    public Animator animator;          
 
     [Header("Ground Checker Settings")]
     private bool isGrounded = false;
@@ -26,23 +26,23 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Определяю ввод с клавиатуры
+        // Detecting keyboard input
         float moveInput = Input.GetAxis("Horizontal");
 
-        // Движение персонажа влево и вправо
+        // Moving the character left and right
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        // Проверяю направление движения и поворачиваем персонажа
+        // Checking the direction of movement and rotating the character
         if (moveInput < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1); // Отражаю по горизонтали
+            transform.localScale = new Vector3(-1, 1, 1); // Reflecting horizontally
         }
         else if (moveInput > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1); // Сбрасываю масштаб
+            transform.localScale = new Vector3(1, 1, 1); // Resetting the scale
         }
 
-        // Задаю значение параметра HorizontalMove в аниматоре
+        // Setting the value of the HorizontalMove parameter in the animator
         animator.SetFloat("HorizontalMove", Mathf.Abs(moveInput));
 
         animator.SetBool("isGrounded", isGrounded);
@@ -51,24 +51,19 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
-        // Проверяю, находимся ли мы на земле
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
-        // Проверяю, нажата ли клавиша S(вниз) и находимся ли мы в воздухе
         if (Input.GetKeyDown(KeyCode.S) && isGrounded == false)
         {
-            // Применяем силу "прыжка" к персонажу, перенаправленную назад - вниз
             rb.AddForce(new Vector2(0f, jumpForce*(-1)));
         }
 
-        // Проверяю, нажата ли клавиша прыжка и находимся ли мы на земле
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            // Применяем силу прыжка к персонажу
             rb.AddForce(new Vector2(0f, jumpForce));
         }
 
-        // Анимация прыжка
+        // Jump animation
         if (isGrounded == false)
         {
             animator.SetBool("Jumping", true);
